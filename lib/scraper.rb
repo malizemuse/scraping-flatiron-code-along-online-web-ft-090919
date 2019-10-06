f2d24eff3f6889932f3c336a30e455da30ed0cd6
gregarious-number-3828 <muse.malize@gmail.com>
2019-10-06 22:41:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
-      end
+      endScraper.new.print_courses
     end
   end
 
